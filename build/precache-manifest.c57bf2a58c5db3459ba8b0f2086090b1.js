7dc75ca664ea0ca5e4c4abc3ef211f508aaf261f
risingbirdsong <heregoingthere@outlook.com>
2020-03-19 18:15:10 -0700

    only want to push build

---
+++ build/precache-manifest.c57bf2a58c5db3459ba8b0f2086090b1.js
@@ -0,0 +1,362 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad45efcd410300b1468507c7e747f7dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d32d26f1bf7ab0750ab4",
+    "url": "/static/css/main.299bba4b.chunk.css"
+  },
+  {
+    "revision": "afea20b85aed97982c66",
+    "url": "/static/js/2.8398d0c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8398d0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d32d26f1bf7ab0750ab4",
+    "url": "/static/js/main.a3d81038.chunk.js"
+  },
+  {
+    "revision": "54636fd8965c59d2305a",
+    "url": "/static/js/runtime-main.0ced885a.js"
+  },
+  {
+    "revision": "f8a87c8eb10eb9391b5f05628cc1ac42",
+    "url": "/static/media/_007.f8a87c8e.png"
+  },
+  {
+    "revision": "0ce5319f0f08a3acdf04a6376c3c92d8",
+    "url": "/static/media/abstractStar.0ce5319f.png"
+  },
+  {
+    "revision": "f4005b4560852a351a5512bdde79cd2d",
+    "url": "/static/media/allColorTangent.f4005b45.png"
+  },
+  {
+    "revision": "aa14335f345c057468a33a6254566c62",
+    "url": "/static/media/amazingWheel.aa14335f.png"
+  },
+  {
+    "revision": "a59092f91d04d4620ac8df697c66016a",
+    "url": "/static/media/angledTentacle.a59092f9.png"
+  },
+  {
+    "revision": "e1559a32d566439f85a2b464832eb591",
+    "url": "/static/media/bezierPlay_a.e1559a32.png"
+  },
+  {
+    "revision": "2249f94c160ed8003a0d9b7ee0cb4f4e",
+    "url": "/static/media/bowtieSwirl.2249f94c.png"
+  },
+  {
+    "revision": "9c3b8f7265c9964e0b688620e2cc6907",
+    "url": "/static/media/bracelet.9c3b8f72.png"
+  },
+  {
+    "revision": "6bb00e2cfa037bd7fa88dc185c812851",
+    "url": "/static/media/bunnySpoon.6bb00e2c.png"
+  },
+  {
+    "revision": "eb774cadc3eb6c301639936c190498be",
+    "url": "/static/media/capsule.eb774cad.png"
+  },
+  {
+    "revision": "05b8d2ce1d3432926d6830067e2b8cf2",
+    "url": "/static/media/christmasStar.05b8d2ce.png"
+  },
+  {
+    "revision": "b3b1daec80628714ff26783e5642204b",
+    "url": "/static/media/classicCar.b3b1daec.png"
+  },
+  {
+    "revision": "30691a3f3658c97566dc192165f18d04",
+    "url": "/static/media/crystalThorns.30691a3f.png"
+  },
+  {
+    "revision": "5d77f6a0d3fc817dfbf1cf4903ef3bce",
+    "url": "/static/media/curves_a.5d77f6a0.png"
+  },
+  {
+    "revision": "bedc8c1394520f644ca2e3c4c8516ddd",
+    "url": "/static/media/cyclone.bedc8c13.png"
+  },
+  {
+    "revision": "c35017132af0a2f325607fb5993b2d31",
+    "url": "/static/media/digitalFan.c3501713.png"
+  },
+  {
+    "revision": "2c6fd5abc17b98f8cebcd22cbd44ab30",
+    "url": "/static/media/digitalHourGlass.2c6fd5ab.png"
+  },
+  {
+    "revision": "a71fce9e7f3705cf76a8da6395e07695",
+    "url": "/static/media/digitalMountains.a71fce9e.png"
+  },
+  {
+    "revision": "deaa67e217a20403f60a26e3c1096319",
+    "url": "/static/media/dottedPurpleTangent.deaa67e2.png"
+  },
+  {
+    "revision": "c27ccfdb8d1de147099bb6c700703c1c",
+    "url": "/static/media/dottedSpiral.c27ccfdb.png"
+  },
+  {
+    "revision": "5cbff5b08eed27dd81032cfdfb24b996",
+    "url": "/static/media/exponentialBoxes.5cbff5b0.png"
+  },
+  {
+    "revision": "0f58d7f6291af2a67371079a2ead35b4",
+    "url": "/static/media/eyeball.0f58d7f6.png"
+  },
+  {
+    "revision": "7d830f1308b9f8a95906f77383d6eaf5",
+    "url": "/static/media/fibby.7d830f13.png"
+  },
+  {
+    "revision": "12bdf4e3abe625de7406fa61ab76dd9e",
+    "url": "/static/media/fractalFlowerBeauty_a.12bdf4e3.png"
+  },
+  {
+    "revision": "11f8e11c9f58ab70fa9e508926b20de3",
+    "url": "/static/media/fractalTips.11f8e11c.png"
+  },
+  {
+    "revision": "8ccf32668bdbc2e734352661ea77b8e4",
+    "url": "/static/media/galacticFlower.8ccf3266.png"
+  },
+  {
+    "revision": "e95b01b1cdd3a0756d0a9c34477ced96",
+    "url": "/static/media/gnarlyStar.e95b01b1.png"
+  },
+  {
+    "revision": "615953b660b929439309c00f0928a0c7",
+    "url": "/static/media/halo.615953b6.png"
+  },
+  {
+    "revision": "e45604f3d6e5f88f6a142849ffc790e1",
+    "url": "/static/media/honeyCombSwirl.e45604f3.png"
+  },
+  {
+    "revision": "6d3b1e4d034cb370b7c98b0b7b1d48ab",
+    "url": "/static/media/hypnoticSwirl.6d3b1e4d.png"
+  },
+  {
+    "revision": "56b363bfb6899987cac8f66308889285",
+    "url": "/static/media/jaggedCyclone.56b363bf.png"
+  },
+  {
+    "revision": "ad480415d90542804d5911ce8b567fa0",
+    "url": "/static/media/jewelNecklaces.ad480415.png"
+  },
+  {
+    "revision": "ad0b497f9c74d22235b51d3ab2bd4a77",
+    "url": "/static/media/jewelStreaks.ad0b497f.png"
+  },
+  {
+    "revision": "5a8da60b06068cee7d9c2a6fd109e64e",
+    "url": "/static/media/lightningBolt_b.5a8da60b.png"
+  },
+  {
+    "revision": "d81a9f0d6c58eb78414d6916e1e080b9",
+    "url": "/static/media/littleBird.d81a9f0d.png"
+  },
+  {
+    "revision": "c6dff086c0dadf11e1b779f2dd3e4344",
+    "url": "/static/media/littleBird_a.c6dff086.png"
+  },
+  {
+    "revision": "f241b04e2afad76f4375012636e8802a",
+    "url": "/static/media/logoB.f241b04e.png"
+  },
+  {
+    "revision": "d380334cef8181243405af951f953f25",
+    "url": "/static/media/logo_a.d380334c.png"
+  },
+  {
+    "revision": "7b3fde94980eec4d2734513e38ec214a",
+    "url": "/static/media/looselyCoiledRug.7b3fde94.png"
+  },
+  {
+    "revision": "d93efba8d55183a7473f3abfec20c7c0",
+    "url": "/static/media/lungs.d93efba8.png"
+  },
+  {
+    "revision": "6d44e3193e5f4f31930055034517e258",
+    "url": "/static/media/nodeStar.6d44e319.png"
+  },
+  {
+    "revision": "5af2c40b4c7db9a6038065de2c577772",
+    "url": "/static/media/nodeStar_b.5af2c40b.png"
+  },
+  {
+    "revision": "670a95e73cb9458db6e3f9870ff02e44",
+    "url": "/static/media/obsidian.670a95e7.png"
+  },
+  {
+    "revision": "eaad8656c2dddece17ec578608102212",
+    "url": "/static/media/origami.eaad8656.png"
+  },
+  {
+    "revision": "a036be072cce77f526be3fa3fb5b5371",
+    "url": "/static/media/paperAirplane.a036be07.png"
+  },
+  {
+    "revision": "6b319132245a441452389f52b54647e7",
+    "url": "/static/media/pentaGram_b.6b319132.png"
+  },
+  {
+    "revision": "bea2b085afa47e7f79e728b43e442102",
+    "url": "/static/media/pentagram_a.bea2b085.png"
+  },
+  {
+    "revision": "08fadf666dd03872d986e875078f9431",
+    "url": "/static/media/prettyAnvil.08fadf66.png"
+  },
+  {
+    "revision": "bfe50c12b9ce9b546761088292bbce9c",
+    "url": "/static/media/prettyJewel_a.bfe50c12.png"
+  },
+  {
+    "revision": "d6f5e441611c104409599b0ef55298d2",
+    "url": "/static/media/purpleMarbledStrand.d6f5e441.png"
+  },
+  {
+    "revision": "72fc4bae541c599fb621678f821e2870",
+    "url": "/static/media/razorCyclone.72fc4bae.png"
+  },
+  {
+    "revision": "253ee1d8f5586e991d3af4ad2f7839b8",
+    "url": "/static/media/razorFlower.253ee1d8.png"
+  },
+  {
+    "revision": "fcee3358b2ec3c68db32648ac6aa32ba",
+    "url": "/static/media/robotStarfish.fcee3358.png"
+  },
+  {
+    "revision": "89ca0b826df3e1dd40cf0700314cf63e",
+    "url": "/static/media/scaleSwirlLogo.89ca0b82.png"
+  },
+  {
+    "revision": "5d8ed7c0507130a7c67e37621f9e4df7",
+    "url": "/static/media/seaShell.5d8ed7c0.png"
+  },
+  {
+    "revision": "f84d0e01a236bdd9b5a17136671c9f36",
+    "url": "/static/media/sharp.f84d0e01.png"
+  },
+  {
+    "revision": "4e6c116655646b6ffb0f04c91d8db5a3",
+    "url": "/static/media/simpleBowtie.4e6c1166.png"
+  },
+  {
+    "revision": "013193be7c3dd07d0d4c45235c84db5f",
+    "url": "/static/media/simpleFlower.013193be.png"
+  },
+  {
+    "revision": "b062d69565fd847bdaef49b054d75729",
+    "url": "/static/media/simpleHexagon.b062d695.png"
+  },
+  {
+    "revision": "f6d58fe52e11f536a3080e58bd2d0a96",
+    "url": "/static/media/simpleLightning.f6d58fe5.png"
+  },
+  {
+    "revision": "d4562e23f617d60a1e24b7e77d30c056",
+    "url": "/static/media/singleStrand.d4562e23.png"
+  },
+  {
+    "revision": "e0486013fa749c811b9129457648adea",
+    "url": "/static/media/skewedBowtie.e0486013.png"
+  },
+  {
+    "revision": "ea39eca4ba3a7f25c155893926a4af98",
+    "url": "/static/media/slingShot.ea39eca4.png"
+  },
+  {
+    "revision": "4a42e176800601e99499be3ed83f5d45",
+    "url": "/static/media/slinkieSwirl.4a42e176.png"
+  },
+  {
+    "revision": "b73cec88e7dc3194600b403e7348e977",
+    "url": "/static/media/slinkyS.b73cec88.png"
+  },
+  {
+    "revision": "0b068e89834035b1f1d10130a42db96f",
+    "url": "/static/media/slinkyTunnel.0b068e89.png"
+  },
+  {
+    "revision": "0577e3238361e7b74d59b2b13bcfa98c",
+    "url": "/static/media/spider.0577e323.png"
+  },
+  {
+    "revision": "e1c875736d0faac523217d14ab730bdd",
+    "url": "/static/media/spikyS.e1c87573.png"
+  },
+  {
+    "revision": "6107c9c4b34d6a747aaa559d386dd5bb",
+    "url": "/static/media/spiralHalo.6107c9c4.png"
+  },
+  {
+    "revision": "ae931c862ca65906662b1ad2cd1e9900",
+    "url": "/static/media/spiralHexagon.ae931c86.png"
+  },
+  {
+    "revision": "2fe0100a74ced0950b180bceca3c5ade",
+    "url": "/static/media/spokedWheel.2fe0100a.png"
+  },
+  {
+    "revision": "f71815aafde51463160ccd4e0073b4bf",
+    "url": "/static/media/strangeBrain.f71815aa.png"
+  },
+  {
+    "revision": "9dcaa6206264636c9124e12055c6f9cd",
+    "url": "/static/media/subtleSpiral_a.9dcaa620.png"
+  },
+  {
+    "revision": "f9f2d39605c837ee19f9acc93e8fbbc1",
+    "url": "/static/media/tangentBeautya.f9f2d396.png"
+  },
+  {
+    "revision": "ca54993f169dc071f4c5c6a7ef729812",
+    "url": "/static/media/targetingSystem.ca54993f.png"
+  },
+  {
+    "revision": "cabdb094df361913f333d3879c283ae4",
+    "url": "/static/media/tentacle.cabdb094.png"
+  },
+  {
+    "revision": "8c467817c22f4fee379a5aec435d4901",
+    "url": "/static/media/throwingStar_a.8c467817.png"
+  },
+  {
+    "revision": "54a260229b6e5ed636f06bb20073e211",
+    "url": "/static/media/throwingStar_b.54a26022.png"
+  },
+  {
+    "revision": "4a550af0eb431bb94e1f4f8f5d30daec",
+    "url": "/static/media/tightlyCoiledRug.4a550af0.png"
+  },
+  {
+    "revision": "7999e5b25912c25f4d31227b4f593c86",
+    "url": "/static/media/trekkie.7999e5b2.png"
+  },
+  {
+    "revision": "08826e380621c1dc1370e608e7e3cf82",
+    "url": "/static/media/triangleSwirl_S.08826e38.png"
+  },
+  {
+    "revision": "2bf61b8ce9ba4b8de08e50008ce0e5a8",
+    "url": "/static/media/twinkle_a.2bf61b8c.png"
+  },
+  {
+    "revision": "dd5c4a4ee6b7e61daf116b809d509740",
+    "url": "/static/media/wavyStrands.dd5c4a4e.png"
+  },
+  {
+    "revision": "51eaafacf2b101bdbbd044058973126e",
+    "url": "/static/media/wrench.51eaafac.png"
+  }
+]);
